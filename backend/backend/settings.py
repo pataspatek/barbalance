64eb1635eb55c1fd977e1098568062d0d52618c0
pataspatek <patrikpatek3@gmail.com>
2026-03-22 22:20:23 +0100

    cros origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'blog',
 ]
 
@@ -59,6 +60,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # your local frontend
+    "https://barbalance.onrender.com/",  # production frontend
 ]
 
 ROOT_URLCONF = 'backend.urls'
