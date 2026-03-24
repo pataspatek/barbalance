bcbd0e59ee181327926b188b41f12794aebaa4ab
pataspatek <patrikpatek3@gmail.com>
2026-03-24 21:48:55 +0100

    build.sh added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
