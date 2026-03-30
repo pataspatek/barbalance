8414b29c69bf5c9075b28ee2823d47d8d73d3d2b
pataspatek <patrikpatek3@gmail.com>
2026-03-30 22:22:57 +0200

    vercel fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
 import './styles/main.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
