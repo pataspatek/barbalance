eb940a01831974291ec27902a1233ab3df715a40
pataspatek <patrikpatek3@gmail.com>
2026-04-07 21:10:55 +0200

    model type error fixed

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from users.models import User
 
 class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='client_profile')
-    phone = models.CharField(max_length=20, blank=True, null=True)
-    company = models.CharField(max_length=255, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    user: models.OneToOneField = models.OneToOneField(User, on_delete=models.CASCADE, related_name='client_profile')
+    phone: models.CharField = models.CharField(max_length=20, blank=True, null=True)
+    company: models.CharField = models.CharField(max_length=255, blank=True, null=True)
+    address: models.CharField = models.CharField(max_length=255, blank=True, null=True)
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.email
